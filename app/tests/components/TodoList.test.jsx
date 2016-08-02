1957f044a992957c861c7d01fe86a5bf3cfcde1a
Zacck <zacck@Zaccks-MacBook-Pro.local>
2016-08-02 13:54:23 +0200

    worked on Testing for TodoList

--- app/tests/components/TodoList.test.jsx
+++ app/tests/components/TodoList.test.jsx
@@ -7,6 +7,7 @@ var TestUtils = require('react-addons-test-utils')
 
 //actual component we need to render
 var TodoList = require('TodoList');
+var TodoItem = require('TodoItem');
 
 //Perform assertions
 describe('TodoList', () =>{
@@ -16,5 +17,23 @@ it('should exist', () => {
 });
 
 //other tests
+it('should render one todo component for each item', () =>{
+  var todos = [
+    {
+      id: 1,
+      text: 'one'
+    },
+    {
+      id: 2,
+      text: 'two'
+    }
+  ];
+
+  var todolist = TestUtils.renderIntoDocument(<TodoList todos={todos}/>);
+  /*components rendered in*/
+  var todosComponents = TestUtils.scryRenderedComponentsWithType(todolist, TodoItem);
+
+  expect(todosComponents.length).toBe(todos.length);
+});
 
 });
