c5c2ff25f7c75cdf9aee08bb71368640ae79bca3
Zacck <coderv63@gmail.com>
2016-08-29 17:16:06 +0200

    add redux store and test actions

--- app/store/configureStore.jsx
+++ app/store/configureStore.jsx
@@ -1,1 +1,17 @@
+var redux = require('redux');
 
+var {searchTextReducer, showCompletedReducer, todosReducer} = require('reducers');
+
+export var configure = () => {
+  var reducer = redux.combineReducers({
+    searchText: searchTextReducer,
+    todos: todosReducer,
+    showCompleted: showCompletedReducer
+  });
+
+  var store = redux.createStore(reducer,
+  redux.compose(window.devToolsExtension ? window.devToolsExtension() : f => f
+  ));
+
+  return store;
+};
