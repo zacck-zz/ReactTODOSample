b1c3e322589e5d96b43a51d714cb25f8b95dbe9c
Zacck <coderv63@gmail.com>
2016-08-31 20:20:18 +0200

    worked on listeners for the data in the data and how to trigger data update actions

--- playground/firebase/index.js
+++ playground/firebase/index.js
@@ -24,38 +24,112 @@ firebaseRef.set({
     age: 26
   },
 
-}).then(()=>{
-  console.log('Set Worked');
-},(e) => {
-  console.log('Set Failed');
-})
-
-//Update information
-firebaseRef.update({
-  isRunning: false,
-  /*multi path update*/
-  'app/name':  'Zacck To Do App'
 });
 
-firebaseRef.child('app').update({
-  name: 'Update Using Child'
-}).then(() => {
-  console.log('update worked');
-}, () => {
-  console.log('update failed');
-});
+var userData = (snapshot) => {
+  console.log('user Value', snapshot.val());
+}
 
-//multipath example
-firebaseRef.update({
-  'app/name': 'Multi Path Update',
-  'user/name':  'Super Coder'
-});
+firebaseRef.child('user').on('value',  userData);
 
-//child example
-firebaseRef.child('app').update({
-  name: 'Child Update Version'
+
+firebaseRef.child('user').update({
+  name: 'name after listener'
 });
 
 firebaseRef.child('user').update({
-  name: 'Straight Up Stacking'
-})
+  name: 'final name'
+});
+
+firebaseRef.child('app').update({
+  name: 'This is an updating app'
+});
+
+
+//fetch data
+// firebaseRef.child('app').once('value').then((snapshot) => {
+//   //console.log('Got Entire Database' , snapshot.val());
+//   console.log('Got App Object', snapshot.key, snapshot.val());
+// }, (e) => {
+//   console.log('Unable to fetch value', e);
+// });
+
+// var logData = snapshot) => {
+//   console.log('Got Value', snapshot.val());
+// };
+//
+// firebaseRef.on('value', logData);
+//
+// firebaseRef.off(logData);
+//
+// firebaseRef.update({isRunning: false});
+// //Update information
+// firebaseRef.update({
+//   isRunning: false,
+//   /*multi path update*/
+//   'app/name':  'Zacck To Do App'
+// });
+//
+// firebaseRef.child('app').update({
+//   name: 'Update Using Child'
+// }).then(() => {
+//   console.log('update worked');
+// }, () => {
+//   console.log('update failed');
+// });
+//
+// //multipath example
+// firebaseRef.update({
+//   'app/name': 'Multi Path Update',
+//   'user/name':  'Super Coder'
+// });
+//
+// //child example
+// firebaseRef.child('app').update({
+//   name: 'Child Update Version'
+// });
+//
+// firebaseRef.child('user').update({
+//   name: 'Straight Up Stacking'
+// });
+//
+//
+//
+// //Remove databaseURL
+// //firebaseRef.remove();
+//
+// //firebaseRef.child('app/name').remove();
+//
+// // firebaseRef.child('app').update({
+// //   version: '2.0',
+// //   name: null
+// // });
+// //
+// // firebaseRef.child('isRunning').remove();
+// //
+// // firebaseRef.child('user/a').update({
+// //   age: null
+// // });
+//
+//
+// //READ DATA
+// firebaseRef.child('app').once('value').then((snapshot) => {
+//   //console.log('Got Entire database',  snapshot.val());
+//   console.log('Got app Object',  snapshot.val());
+// }, (e) => {
+//   console.log('Unable to fetch value', e);
+// });
+//
+//
+// //listens for changes
+//
+// firebaseRef.on('value',(snapshot) => {
+//   console.log('ref updated',snapshot.val());
+// });
+//
+// var logData = (snapshot) => {
+//   console.log('ref updated',snapshot.val());
+// };
+//
+// firebaseRef.off();
+// firebaseRef.update({isRunning: false});
